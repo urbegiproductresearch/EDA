8b68dc9f4cd5476f3c9b7bb066038305fbcf8000
USILAB <admin@ekaidepd.com>
2026-02-18 10:03:13 +0100

    Update altxor.py

--- procesamiento_resources/config/altxor.py
+++ procesamiento_resources/config/altxor.py
@@ -1,1 +1,96 @@
+CONFIG = {
+
+    "nombre_comunidad": "altxor",
+
+    "generos": ["Hombre", "Mujer", "No binario"],
+
+    "edades": [
+        "20-29 años",
+        "30-39 años",
+        "40-49 años",
+        "50-59 años",
+        "60-69 años",
+        "70-80 años",
+        "Más de 80 años"
+    ],
+
+    "roles": [
+        "Ciudadanos",
+        "Empleados públicos",
+        "Asesores",
+        "Cuidadores"
+    ],
+
+    "ambitos": [
+        "Empresas",
+        "Entidades de formación",
+        "Instituciones públicas"
+    ],
+
+    "canales": [
+        "Salud y autonomía",
+        "Mayores expatriados",
+        "Cooperación del desarrollo",
+        "Primeros pasos en Altxor Digital",
+        "Experiencias vitales",
+        "Iniciativas colectivas",
+        "Seguridad y protección",
+        "Ocio y actividad física",
+        "Ecología, medioambiente y tecnología",
+        "Participación, convivencia y voluntariado"
+    ],
+
+    "tipos_evento": [
+        "Feria",
+        "Jornada",
+        "Reunión",
+        "Charla",
+        "Taller"
+    ],
+
+    "tipos_contenido": [
+        "Noticias",
+        "Informes",
+        "Artículos",
+        "Casos de éxito",
+        "Premios y reconocimientos",
+        "Mesas Redondas",
+        "Entrevistas",
+        "Otros",
+        "Faq",
+        "Glosario",
+        "Monográfico"
+    ],
+
+    "areas": [
+        "Economía",
+        "Vivienda",
+        "Entornos amigables",
+        "Justicia",
+        "Cuestiones sociales, afectivas o comunitarias"
+    ],
+
+    "formatos": [
+        "Online",
+        "Presencial",
+        "Mixto",
+        "Materiales de apoyo"
+    ],
+
+    "tipos_espacio": [
+        "Residencias",
+        "Centros de día"
+    ],
+
+    "perfiles_con_area": [
+        "Productos o servicios",
+        "Noticia",
+        "Ayuda",
+        "Evento",
+        "Recurso web",
+        "Oportunidad",
+        "Reto",
+        "Otros"
+    ],
+}
 
